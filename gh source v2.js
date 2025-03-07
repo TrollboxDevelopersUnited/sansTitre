404b0af343a578b7f0982232fc411a38c094ebce
UntitledTrollbox <piskovtima0@gmail.com>
2025-03-07 20:22:24 +0300

    beta 10 official code update

--- gh source v2.js
+++ gh source v2.js
@@ -21,22 +21,22 @@ const socket = io("http://windows93.net:8081", {
 	}
 });
 
-        socket.emit("user joined", "sansTitre [gh!]", "#ff66ff", "", "");
+        socket.emit("user joined", "sansTitre [gh!]", "#ff55ff", "", "");
 
         socket.on("message", function(data) {
 
             if (data.msg == "hello") {
-                socket.send(he.decode("hello "+data.nick+""))
+                socket.send(he.decode("hello "+data.nick+"!"))
             }
 
             if (data.msg == "hi") {
-                socket.send(he.decode("hi "+data.nick+""))
+                socket.send(he.decode("hi "+data.nick+"!"))
             }
             if (data.msg == "gh!help") {
-                socket.send(he.decode("sanstitre, literally a user index\nversion - beta 10 early\ngh!untitled - creator of the bot\ngh!qpfm - cute bot :3\ngh!hain - maker of cute bot :3\ngh!word - trigger applevinyl's word command thingy\ngh!mage - tb og\npage 1/5\ndo gh!help <page number> for page"))
+                socket.send(he.decode("sanstitre, literally a user index\nversion - beta 10 official\ngh!untitled - creator of the bot\ngh!qpfm - known as jqb nowadays\ngh!hain - maker of jqb\ngh!word - trigger applevinyl's word command thingy\ngh!wishmaker - tb og\npage 1/5\ndo gh!help <page number> for page"))
             }
             if (data.msg == "gh!help 2") {
-                socket.send(he.decode("gh!lawnfin - nice bot\ngh!bensav - silly guy\ngh!applevinyl - qpfm's husband\ngh!big - bigweirdo\ngh!redf - brazillian guy\npage 2/5\ndo gh!help <page number> for page"))
+                socket.send(he.decode("gh!flapplin - nice bot\ngh!bensav - silly guy\ngh!applevinyl - qpfm's husband\ngh!big - bigweirdo\ngh!redf - brazillian guy\npage 2/5\ndo gh!help <page number> for page"))
             }
             if (data.msg == "gh!help 3") {
                 socket.send(he.decode("gh!rnpg - generate a random polygon from 1 to 100 using scarbot\ngh!ar - we do not talk about him.\ngh!dell - master coder\ngh!wowaka - hain's favorite vocaloid producer\ngh!rng - a random random generator from 1 to 255 (255 - win)\npage 3/5\ndo gh!help <page number> for page"))
@@ -48,69 +48,69 @@ const socket = io("http://windows93.net:8081", {
                 socket.send(he.decode("gh!sns - my socials (unfinished)\npage 5/5\ndo gh!help <page number> for page"))
             }
             if (data.msg == "gh!qpfm") {
-                socket.send(he.decode("cute bot :3"))
+                socket.send(he.decode("jqb"))
             }
             if (data.msg == "gh!untitled") {
-                socket.send(he.decode("he made me, BOTU, TBB scripts and BTPOT"))
+                socket.send(he.decode("he made me, BOTU, the TBB scripts and BTPOT"))
             }
             if (data.msg == "gh!hain") {
-                socket.send(he.decode("maker of the cute bot :3"))
+                socket.send(he.decode("maker of jqb"))
             }
             if (data.msg == "gh!word") {
                 socket.send(he.decode("{:word}"))
             }
-            if (data.msg == "gh!mage") {
+            if (data.msg == "gh!wishmaker") {
                 socket.send(he.decode("trollbox og, was on trollbox since 2017"))
             }
-            if (data.msg == "gh!lawnfin") {
+            if (data.msg == "gh!flapplin") {
                 socket.send(he.decode("one time he was a copy of nyfelnix/xeonix, now a nice bot"))
             }
             if (data.msg == "gh!bensav") {
-                socket.send(he.decode("silly user"))
+                socket.send(he.decode("balls with wallss"))
             }
             if (data.msg == "gh!applevinyl") {
-                socket.send(he.decode("silly user's bot and also qpfm's husband"))
+                socket.send(he.decode("silly user's bot and also qpfm's husband, somehow"))
             }
             if (data.msg == "gh!big") {
-                socket.send(he.decode("another trollbox og, since 2019"))
+                socket.send(he.decode("another trollbox og, joined in 2019"))
             }
             if (data.msg == "gh!redf") {
-                socket.send(he.decode("brazillian osc fan that made TBB"))
+                socket.send(he.decode("brazillian osc fan that made tbb"))
             }
             if (data.msg == "gh!rnpg") {
                 socket.send("&polygon " + Math.floor(Math.random() * 100))
             }
             if (data.msg == "gh!ar") {
-                socket.send(he.decode("don't be like that"))
+                socket.send(he.decode("don't be like that guy bruh"))
             }
             if (data.msg == "gh!dell") {
-                socket.send(he.decode("master coder according to the ---- wiki"))
+                socket.send(he.decode("master coder"))
             }
             if (data.msg == "gh!wowaka") {
-                socket.send(he.decode("hain's favorite vocaloid producer"))
+                socket.send(he.decode("hain's favorite vocaloid producer, irrelevant"))
             }
             if (data.msg == "gh!rng") {
                 socket.send("your number is: " + Math.floor(Math.random() * 255))
             }
             if (data.msg == "how old are you") {
-                socket.send(he.decode("omg guys "+data.nick+" is a creep now whar"))
+                socket.send(he.decode("omg guys "+data.nick+" is a creep"))
             }
-            if (data.msg == "gh!kurt") {
+            if (data.msg == "gh!kurt") {b
                 socket.send(he.decode("just kurt"))
             }
             if (data.msg == "gh!miweoro") {
                 socket.send(he.decode("made the second best economy bot"))
             }
             if (data.msg == "gh!econbot") {
-                socket.send(he.decode("the best ecenomy bot"))
+                socket.send(he.decode("the best ecenomy bot, dead"))
             }
             if (data.msg == "gh!event") {
-                socket.send(he.decode("WE'RE BACK!\nthe sanstitre bot is officially BACK after 3-4 months!!!"))
+                socket.send(he.decode("beta 10 released\nyippee"))
             }
             if (data.msg == "gh!bonziworld") {
                 socket.send(he.decode("fuck them LMFAOOOOO"))
             }
             if (data.msg == "gh!sns") {
-                socket.send(he.decode("https://bsky.app/profile/theguyever47.bsky.social - bluesky"))
+                socket.send(he.decode("https://bsky.app/profile/theguyever47.bsky.social - bluesky\nhttps://github.com/TrollboxDevelopersUnited/sansTitre - my bot code\nUntitledTrollbox - youtube"))
             }
             })
